8bfbf04db87404cf0c52199d00a9941c9a2e4a5a
Tamara Stefanovic <stefanov.tamara@gmail.com>
2018-10-09 20:06:46 -0400

    add horoscope description to details page

--- lib/horoscope_route.dart
+++ lib/horoscope_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'models/horoscope.dart';
 
 class HoroscopeRoute extends StatelessWidget {
   final String name;
@@ -15,17 +16,31 @@ class HoroscopeRoute extends StatelessWidget {
        assert(icon != null);
 
   @override
-
   Widget build(BuildContext context) {
     return Container(
-      color: color,
+      color: Colors.transparent,
       margin: EdgeInsets.all(8.0),
       padding: EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text(
-            name,
-            style: Theme.of(context).textTheme.headline
+          new Text(
+            'Today\s Horoscope',
+            style: Theme.of(context).textTheme.display1,
+          ),
+          Center(
+            child: FutureBuilder<Horoscope>(
+              future: fetchHoroscope(name),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.description);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+              }
+            ),
           )
         ],
       )
